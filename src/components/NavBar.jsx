eabb3fd108db0a088239679416387e6c8ad6f5cc
Clark Ngo <clarkngo@gmail.com>
2025-10-27 17:41:50 -0700

    chore: add How to Teach link to NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,6 +7,7 @@ export default function NavBar() {
         <Link to="/" className="brand">AI Unplugged</Link>
         <div className="nav-links">
           <Link to="/what-is-ai">What is AI?</Link>
+          <Link to="/how-to-teach">How to Teach</Link>
           <Link to="/machine-learning">Machine Learning</Link>
           <Link to="/computer-vision">Computer Vision</Link>
           <Link to="/nlp">NLP</Link>
